612226c5bcf6c91a1ece13a9ea07d52da9e35900
chrisraff <crharfifs@gmail.com>
2020-07-17 21:18:36 -0400

    added flying controller

--- js/game.js
+++ js/game.js
@@ -1,11 +1,18 @@
 import * as THREE from 'https://unpkg.com/three@0.118.3/build/three.module.js';
+import { FlyControls } from 'https://unpkg.com/three@0.118.3/examples/jsm/controls/FlyControls.js';
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
+// renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const controls = new FlyControls(camera, renderer.domElement);
+controls.dragToLook = true;
+controls.movementSpeed = 1;
+controls.rollSpeed = 1;
 
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -17,8 +24,7 @@ camera.position.z = 5;
 var animate = function () {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    controls.update(0.01);
 
     renderer.render( scene, camera );
 };
