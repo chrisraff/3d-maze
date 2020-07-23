4b6dff42f7c8b34ff6780c7c7a42c3806a3ff98f
chrisraff <crharfifs@gmail.com>
2020-07-23 00:44:20 -0400

    made init function

--- js/game.js
+++ js/game.js
@@ -57,48 +57,64 @@ controls.addEventListener( 'unlock', function() {
     blocker.style.display = 'block';
 } );
 
-camera.position.set( maze.getOffset(1), maze.getOffset(1), maze.getOffset(-2))
-camera.lookAt(maze.getOffset(1), maze.getOffset(1), 0);
-
-// build maze
+// maze variables
 var mazeSize = 5;
 var mazeData = maze.generateMaze(mazeSize);
-var geometry = new THREE.BoxGeometry();
-for (var i = 0; i < mazeData.length; i++) {
-    for (var j = 0; j < mazeData[i].length; j++) {
-        for (var k = 0; k < mazeData[i].length; k++) {
-            if (    !mazeData[i][j][k] || 
-                    (i!=0 && i!=mazeSize*2 && j!=0 && j!=mazeSize*2 && k!=0 && k!=mazeSize*2 && // if we're inside...
-                        i%2==0 && j%2==0 && k%2==0)) // don't create unseen blocks
-                continue;
-            
-            let iWidth = maze.getWidth(i);
-            let jWidth = maze.getWidth(j);
-            let kWidth = maze.getWidth(k);
-
-            // only large walls get color
-            let colorful = false;
-            if (iWidth + jWidth + kWidth >= 2 * maze.majorWidth + maze.minorWidth)
-                colorful = true;
-
-            // let material = new THREE.MeshLambertMaterial( { color: `hsl(${Math.floor(Math.random() * 360)},${colorful ? 100 : 0}%,${colorful ? 50 : 10}%)` } );
-            let material = new THREE.MeshPhongMaterial( { color: colorful ? `rgb(${
-                Math.floor( 255 * i/(mazeSize*2+1) ) },${
-                Math.floor( 255 * j/(mazeSize*2+1) ) },${
-                Math.floor( 255 * k/(mazeSize*2+1) ) })` : `hsl(0, 0%, 10%)`,
-                opacity: 0.85, transparent: colorful, map: colorful ? gridTexture : null, specularMap: colorful ? gridSpecMap : null} );
-            let block = new THREE.Mesh( geometry, material );
-            block.scale.set( iWidth, jWidth, kWidth );
-            block.position.set( maze.getOffset(i), maze.getOffset(j), maze.getOffset(k) );
-
-            scene.add( block );
-        }
-    }
-}
-
-const collisionDistance = 0.25;
+var mazeGroup = new THREE.Group();
+scene.add( mazeGroup );
+// collisions
 var mazePosNear = null; // closer to 0,0,0 (-)
 var mazePosFar = null;
+
+function init(size=mazeSize) {
+
+    mazePosNear = null;
+    mazePosFar = null;
+
+    camera.position.set( maze.getOffset(1), maze.getOffset(1), maze.getOffset(-2));
+    camera.lookAt(maze.getOffset(1), maze.getOffset(1), 0);
+
+    mazeGroup.remove(...mazeGroup.children);
+
+    // build maze
+    mazeSize = size;
+    mazeData = maze.generateMaze(mazeSize);
+    let geometry = new THREE.BoxGeometry();
+    for (let i = 0; i < mazeData.length; i++) {
+        for (let j = 0; j < mazeData[i].length; j++) {
+            for (let k = 0; k < mazeData[i].length; k++) {
+                if (    !mazeData[i][j][k] || 
+                        (i!=0 && i!=mazeSize*2 && j!=0 && j!=mazeSize*2 && k!=0 && k!=mazeSize*2 && // if we're inside...
+                            i%2==0 && j%2==0 && k%2==0)) // don't create unseen blocks
+                    continue;
+                
+                let iWidth = maze.getWidth(i);
+                let jWidth = maze.getWidth(j);
+                let kWidth = maze.getWidth(k);
+
+                // only large walls get color
+                let colorful = false;
+                if (iWidth + jWidth + kWidth >= 2 * maze.majorWidth + maze.minorWidth)
+                    colorful = true;
+
+                // let material = new THREE.MeshLambertMaterial( { color: `hsl(${Math.floor(Math.random() * 360)},${colorful ? 100 : 0}%,${colorful ? 50 : 10}%)` } );
+                let material = new THREE.MeshPhongMaterial( { color: colorful ? `rgb(${
+                    Math.floor( 255 * i/(mazeSize*2+1) ) },${
+                    Math.floor( 255 * j/(mazeSize*2+1) ) },${
+                    Math.floor( 255 * k/(mazeSize*2+1) ) })` : `hsl(0, 0%, 10%)`,
+                    opacity: 0.85, transparent: colorful, map: colorful ? gridTexture : null, specularMap: colorful ? gridSpecMap : null} );
+                let block = new THREE.Mesh( geometry, material );
+                block.scale.set( iWidth, jWidth, kWidth );
+                block.position.set( maze.getOffset(i), maze.getOffset(j), maze.getOffset(k) );
+
+                mazeGroup.add( block );
+            }
+        }
+    }
+
+};
+
+const collisionDistance = 0.25;
 var axes = {x: 0, y: 1, z: 2};
 function checkCollisionOnAxis(majorAxis, othA0, othA1, mazePosRelevant, newMazePosRelevant, mazePosOther, sign) {
     let min0 = Math.min(mazePosRelevant[othA0], mazePosOther[othA0]);
@@ -133,7 +149,7 @@ function checkCollisionOnAxis(majorAxis, othA0, othA1, mazePosRelevant, newMazeP
     } else {
         mazePosRelevant[majorAxis] += sign;
     }
-}
+};
 function collisionUpdate() {
     let nearPos = new THREE.Vector3();
     nearPos.copy(camera.position);
@@ -176,7 +192,7 @@ function collisionUpdate() {
 
     mazePosNear = newMazePosNear;
     mazePosFar = newMazePosFar;
-}
+};
 
 var animate = function () {
     let delta = clock.getDelta();
@@ -190,4 +206,5 @@ var animate = function () {
     renderer.render( scene, camera );
 };
 
+init();
 animate();
