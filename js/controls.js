817a8ff2d6f80a2bdfaf94fa8a1ca0c8ac9723c6
chrisraff <crharfifs@gmail.com>
2022-04-12 00:45:05 -0400

    Reset Move State on Exiting PointerLock

--- js/controls.js
+++ js/controls.js
@@ -179,6 +179,10 @@ var FlyPointerLockControls = function ( object, domElement ) {
 
             scope.isLocked = false;
 
+            scope.moveState = { up: 0, down: 0, left: 0, right: 0, forward: 0, back: 0, pitchUp: 0, pitchDown: 0, yawLeft: 0, yawRight: 0, rollLeft: 0, rollRight: 0 };
+            scope.updateMovementVector();
+            scope.updateRotationVector();
+
         }
     };
 
